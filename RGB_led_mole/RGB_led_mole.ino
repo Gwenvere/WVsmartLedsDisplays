ac0e3ce9757a2f3494863c921fe2af1914f7fead
Gwenvere <barbara.ameloot@gmail.com>
2019-04-23 14:50:50 +0200

    added dynamic threshold

--- RGB_led_mole/RGB_led_mole.ino
+++ RGB_led_mole/RGB_led_mole.ino
@@ -1,6 +1,9 @@
 int redPin= 1;
 int greenPin = 2;
 int bluePin = 3;
+int prevAverage = 999;
+int theshAdd = 15;
+
 
 int irPin = 1;
 #define COMMON_ANODE
@@ -16,6 +19,7 @@ void setup() {
 
   Serial.begin(9600);
   clearPin(irPin);
+  delay(100);
   
 }
 void loop() {
@@ -32,14 +36,18 @@ void loop() {
 
 void activated(){
 
-  
+  int threshold = getBaseValue() + theshAdd;
   setColor(255, 0, 0); // Red Color
+  Serial.println("Threshold:");
+  Serial.println(threshold);
+  Serial.println("");
+  
   ledDelay.start(3000);
   boolean notCorrected = true;
   while (!ledDelay.isFinished()&& notCorrected){
     int val = analogRead(irPin);
-    Serial.println(val);
-    if (val > 380){
+    //Serial.println(val);
+    if (val > threshold){
     setColor(0,0,255); // Blue Color
     delay(400);
     setColor(0,255,0);//Green
@@ -52,25 +60,35 @@ void activated(){
   }
 }
 
-int getBaseValue(){
+int getBaseValue(){ //get a base reading of the IR value
 
-  const int numReadings = 10;
-
+  int numReadings = 10;           //the number of readings
   int readings[numReadings];      // the readings from the analog input
   int readIndex = 0;              // the index of the current reading
-  int total = 0;                  // the running total
   int average = 0;                // the average
 
-  for (int i = 0; i < numReadings; i++) {
-    
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) { //get readings
     readings[thisReading] = analogRead(irPin);
     delay(50);
   }
-
-  
+  average = (int) getAverage (readings, numReadings);
+  //Serial.println(average);
+  //Serial.println(prevAverage);
+  if (average > prevAverage + 10){ //the pen was probably still pointed at the led
+    average = prevAverage;
+  }
+  prevAverage = average;
+  return average;
  
 }
 
+float getAverage (int * array, int len)  
+{
+  long sum = 0L ;  
+  for (int i = 0 ; i < len ; i++)
+    sum += array [i] ;
+  return  ((float) sum) / len ;  
+}
 void flicker(){
   for ( int i = 0; i<5; i++){
     setColor(255,180,0);//orange
